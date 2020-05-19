b354a5f8d54d094473abaad1cbc4a581a1a2b623
ash37373 <ash37373@gmail.com>
2020-05-20 02:00:35 +0900

    Initial commit

---
+++ basics/numbers.js
@@ -0,0 +1,16 @@
+/* let age = 35;
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears); */
+
+//Challenge area
+
+// studentScore 18
+// maxScore 20
+// percent 90
+
+//print the percent
+let studentScore = 18;
+let maxScore = 20;
+let percent = (studentScore / maxScore) * 100;
+console.log(percent);
---
+++ basics/strings.js
@@ -0,0 +1,10 @@
+let firstName = "Ahn";
+let lastName = "sunghwan";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//challenge
+let city = "Seoul";
+let country = "South Korea";
+let location = city + ", " + country;
+console.log(location);
